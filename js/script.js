8117c3eebfdfab1ddb7ae26de21f84de4b6f414d
Alex <alexmikhajlovic@gmail.com>
2021-02-10 15:13:37 +0100

    Init

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var app = new Vue ({
+
+    el: ('#app'),
+    data: {
+        apiKey: '302a53c30542f331c5da295c0142c319',
+        inputQuery: '',
+        language: 'en-US',
+        visible: true
+    },
+    methods: {
+
+        search() {
+            axios
+            .get('https://api.themoviedb.org/3/search/movie',{
+                params: {
+                    api_key: this.apiKey,
+                    query: this.inputQuery,
+                    language: this.language
+                }
+            })
+            .then((result) => {
+                console.log(result.data.results);
+            })
+            .catch((error) => alert('ERROR'));
+        },
+
+        // Change Language
+        ita() {
+            this.language = 'it-IT';
+            console.log(this.language);
+        },
+        eng() {
+            this.language = 'en-US';
+            console.log(this.language);
+        },
+    },
+    
+});
